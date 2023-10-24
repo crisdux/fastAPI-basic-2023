cc89f0443afb715ac5f5de29d1cdce5ee7510f02
crisdux <crisfer.4217@gmail.com>
2023-10-24 17:20:12 -0400

    usar el verbo post

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
+
+from datetime import datetime
+from typing import Optional, Text
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+from uuid import uuid4 as uuid
+
+
 
 app = FastAPI()
+
+class Post(BaseModel):
+    id: Optional[str] = str(uuid())
+    title:str
+    author:str
+    content: Text
+    created_at:datetime = datetime.now()
+    published: bool = False
 
 posts = []
 @app.get("/")
@@ -12,3 +29,9 @@ async def root():
 async def get_posts():
     return posts
 
+@app.post("/posts")
+async def save_post(post:Post):
+    # post.id = str(uuid())
+    print(post.model_dump())
+    posts.append(post.model_dump())
+    return "recibido"
