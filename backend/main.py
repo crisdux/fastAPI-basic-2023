94f3a7eecbd182ef3857c5b6e72ccb989d9df31c
crisdux <crisfer.4217@gmail.com>
2023-10-24 18:49:18 -0400

    buscar un objeto por id

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,25 @@
 
 from datetime import datetime
 from typing import Optional, Text
+import uuid
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi import HTTPException
 
 
-from uuid import uuid4 as uuid
+# from uuid import uuid4
 
 
 
 app = FastAPI()
 
 class Post(BaseModel):
-    id: Optional[str] = str(uuid())
+    id: Optional[str]
     title:str
     author:str
     content: Text
-    created_at:datetime = datetime.now()
-    published: bool = False
+    # created_at:datetime = datetime.now()
+    # published: bool = False
 
 posts = []
 @app.get("/")
@@ -31,7 +33,15 @@ async def get_posts():
 
 @app.post("/posts")
 async def save_post(post:Post):
-    # post.id = str(uuid())
+    post.id = str(uuid.uuid4())
+    print(post.id)
     print(post.model_dump())
     posts.append(post.model_dump())
-    return "recibido"
+    return posts[-1]
+
+@app.get("/posts/{post_id}")
+async def get_post(post_id:str):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=404, detail="Post not found")
