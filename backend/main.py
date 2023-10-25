5b610da7c3e35c6650105631ff4b2cc2b6d6c531
crisdux <crisfer.4217@gmail.com>
2023-10-24 22:00:36 -0400

    usar el verbo post delete

--- backend/main.py
+++ backend/main.py
@@ -6,11 +6,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import HTTPException
 
-
-# from uuid import uuid4
-
-
-
 app = FastAPI()
 
 class Post(BaseModel):
@@ -18,8 +13,8 @@ class Post(BaseModel):
     title:str
     author:str
     content: Text
-    # created_at:datetime = datetime.now()
-    # published: bool = False
+    created_at:datetime = datetime.now()
+    published: bool = False
 
 posts = []
 @app.get("/")
@@ -34,8 +29,6 @@ async def get_posts():
 @app.post("/posts")
 async def save_post(post:Post):
     post.id = str(uuid.uuid4())
-    print(post.id)
-    print(post.model_dump())
     posts.append(post.model_dump())
     return posts[-1]
 
@@ -44,4 +37,12 @@ async def get_post(post_id:str):
     for post in posts:
         if post["id"] == post_id:
             return post
+    raise HTTPException(status_code=404, detail="Post not found")
+
+@app.delete("/posts/{post_id}")
+async def delete_post(post_id:str):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+            return {"message": "Post deleted successfully"}
     raise HTTPException(status_code=404, detail="Post not found")
